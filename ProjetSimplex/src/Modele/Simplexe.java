20a81cbc39442d186981d445ab6d7bca1ab881b6
Westtunger <westtunger@gmail.com>
2017-11-23 13:13:48 +0100

    Commit initial ~Nicolas

---
+++ ProjetSimplex/src/Modele/Simplexe.java
@@ -0,0 +1,51 @@
+package Modele;
+
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
+
+import Utilitaire.Matrice;
+
+public class Simplexe 
+{
+	Matrice matrice;
+	Simplexe instance;
+
+	private Simplexe()
+	{
+		
+	}
+	
+	public Simplexe getInstance()
+	{
+		if(instance == null)
+			instance = new Simplexe();
+	
+		return instance;
+	}
+	
+	public void setMatrice(Matrice matrice) 
+	{
+		this.matrice = matrice;
+	}
+	
+	public void resoudre()
+	{
+		while(matrice.objectifEstPositive())
+		{
+			Point pivot = trouverPivot();
+			Gauss.rendrePivotUnitaire(matrice,pivot);
+			//Ecriture dans les observer
+		}
+		
+		//Le resultat final est disponible
+	}
+	
+	public Point trouverPivot()
+	{
+		int lignePosPivot = matrice.getMinLignePos(matrice.longueur()-1);
+		int colonnePosPivot = matrice.getMaxColonePos(matrice.hauteur()-1);
+		
+		return new Point(lignePosPivot, colonnePosPivot);
+	}
+}
---
+++ ProjetSimplex/src/Utilitaire/Matrice.java
@@ -0,0 +1,31 @@
+package Utilitaire;
+
+public class Matrice 
+{
+
+	public boolean objectifEstPositive() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public int longueur() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int hauteur() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getMinLignePos(int i) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getMaxColonePos(int i) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
