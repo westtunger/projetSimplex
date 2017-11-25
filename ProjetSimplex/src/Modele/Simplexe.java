eb0d524a490339ad5cd8d67a1b55b0e3dfb881cc
Westtunger <westtunger@gmail.com>
2017-11-25 17:47:11 +0100

    Première itération de travail dans la Classe Simplex ~Nicolas J'ai tester avec les exercices 6.1,6.2 et 6.3 et tous fonctionnent.

--- ProjetSimplex/src/Modele/Simplexe.java
+++ ProjetSimplex/src/Modele/Simplexe.java
@@ -1,49 +1,82 @@
 package Modele;
 
-import java.awt.Point;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.stream.Collectors;
+import Utilitaire.Matrice2;
 
-import Utilitaire.Matrice;
-
-public class Simplexe 
+public abstract class Simplexe 
 {
-	Matrice matrice;
-	Simplexe instance;
-
-	private Simplexe()
-	{
-		
-	}
 	
-	public Simplexe getInstance()
+	public static String resoudre(Matrice2 matrice)
 	{
-		if(instance == null)
-			instance = new Simplexe();
+		int lignePivot, colonnePivot;
+		String resultat = "";
 	
-		return instance;
-	}
-	
-	public void setMatrice(Matrice matrice) 
-	{
-		this.matrice = matrice;
-	}
-	
-	public void resoudre()
-	{
 		while(matrice.objectifEstPositive())
 		{
-			Point pivot = trouverPivot();
+			colonnePivot = trouverColonnePivot(matrice);
+			lignePivot = trouverLignePivot(matrice,colonnePivot);
+			System.out.println("Ligne du pivot : "+lignePivot);
+			System.out.println("Colonne du pivot : "+colonnePivot);
+			matrice = rendrePivotUnitaire(matrice, lignePivot, colonnePivot);
+			matrice = faireRentrerPivotDansLaBase(matrice, lignePivot, colonnePivot);
+			System.out.println(matrice);
 		}
 		
-		//Le resultat final est disponible
+		return resultat;
 	}
 	
-	public Point trouverPivot()
+	private static int trouverLignePivot(Matrice2 matrice, int colonnePivot)
 	{
-		int lignePosPivot = matrice.getMinLignePos(matrice.getLargeur()-1);
-		int colonnePosPivot = matrice.getMaxColonnePos(matrice.getHauteur()-1);
+		int lignePivot = 0;
+		double valeurLigne;
+		double min = matrice.getValeur(0, matrice.getLargeur()-1)/matrice.getValeur(0, colonnePivot);
 		
-		return new Point(lignePosPivot, colonnePosPivot);
+		for(int i = 1; i < matrice.getHauteur()-1;i++)
+		{
+			valeurLigne = matrice.getValeur(i, matrice.getLargeur()-1)/matrice.getValeur(i, colonnePivot);
+			if(valeurLigne < min || min < 0)
+			{
+				min = valeurLigne;
+				lignePivot = i;
+			}
+		}
+		
+		return lignePivot;
+	}
+	
+	private static int trouverColonnePivot(Matrice2 matrice)
+	{
+		int colonnePivot;
+		
+		colonnePivot = matrice.getMaxLignePos(matrice.getHauteur()-1);
+		
+		return colonnePivot;
+	}
+	
+	private static Matrice2 rendrePivotUnitaire(Matrice2 matrice, int lignePivot, int colonnePivot)
+	{
+		double div = matrice.getValeur(lignePivot, colonnePivot);
+		
+		matrice.setLigne(lignePivot,matrice.getLigne(lignePivot).stream().map(valeur -> valeur/div).collect(Collectors.toList()));
+		
+		return matrice;
+	}
+	
+	private static Matrice2 faireRentrerPivotDansLaBase(Matrice2 matrice, int lignePivot, int colonnePivot) 
+	{
+		for(int i = 0; i < matrice.getHauteur();i++)
+		{
+			if(i != lignePivot)
+			{
+				double mult = matrice.getValeur(i, colonnePivot) / matrice.getValeur(lignePivot, colonnePivot);
+				
+				for(int j = 0; j < matrice.getLargeur();j++)
+				{
+					matrice.setValeur(i, j, matrice.getValeur(i, j)-mult*matrice.getValeur(lignePivot, j));
+				}
+			}
+		}
+		
+		return matrice;
 	}
 }
